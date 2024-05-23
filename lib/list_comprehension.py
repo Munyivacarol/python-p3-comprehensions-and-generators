7e27b8a8b53573a6fe4188205f8b819c0550190f
Carolyne munyiva <munyivacarolyne571@gmail.com>
2024-05-23 21:52:40 +0300

    complete list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    new_list=[n for n in num_list if n % 2 == 0]
+    return new_list
+    
     pass
 
 def make_exclamation(sentence_list):
+    new_sentence=[n + "!" for n in sentence_list]
+    return new_sentence
     pass
